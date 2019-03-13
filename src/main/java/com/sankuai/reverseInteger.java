f61560312e3ee07d21dd95be71100efcf4475c17
lbhwjs1993 <1459247241@qq.com>
2019-03-13 19:24:25 +0800

    刷题

---
+++ src/main/java/com/sankuai/reverseInteger.java
@@ -0,0 +1,22 @@
+package com.sankuai;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class reverseInteger {
+    public static void main(String[] args) {
+        int a = Integer.MIN_VALUE + 1;
+        int b = reverse(a);
+        System.out.println(b);
+
+    }
+
+    public static int reverse(int a) {
+        int b = 0;
+        while (a != 0) {
+            b = b * 10 + a % 10;
+            a /= 10;
+        }
+        return (b > Integer.MAX_VALUE || b < Integer.MIN_VALUE) ? 0 : b;
+    }
+}
---
+++ src/main/java/com/sankuai/testTwoSum.java
@@ -0,0 +1,24 @@
+package com.sankuai;
+
+public class testTwoSum {
+    public static void main(String[] args) {
+        int[] a = {2,7,11,15};
+        int[] arrays1 = tweSum(a, 9);
+        System.out.println(arrays1[0] + "," + arrays1[1]);
+
+    }
+
+    public static int[] tweSum(int[] nums, int target) {
+        int[] arrays1 = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    nums[0] = i;
+                    nums[1] = j;
+                }
+            }
+        }
+
+        return arrays1;
+    }
+}
